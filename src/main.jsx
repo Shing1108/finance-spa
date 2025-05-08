8c2147b8332cd43fb53bd617cbdbff459f4fc575
Oscar <moneymonitor1108@gmail.com>
2025-05-09 02:24:48 +0800

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+
 import { useFinanceStore } from "./store/financeStore";
 window.useFinanceStore = useFinanceStore;
 
